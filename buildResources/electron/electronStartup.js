b77c5baf6915962c974227b52cfc5289a41c5342
Tidic <tidic84@gmail.com>
2025-08-08 11:15:52 +0200

    revert format changes

--- buildResources/electron/electronStartup.js
+++ buildResources/electron/electronStartup.js
@@ -27,19 +27,19 @@ const { spawn, execSync } = require('child_process');
 const path = require('path');
 
 let serverProcess = null;
-app.name = 'Pithekos';
+app.name = '${APP_NAME}';
 const port = '19119';
 let canClose = true;
 
 // Function to check if server is running (on port)
 function isServerRunning() {
-    try {
-        // macOS & Linux: use lsof; Windows would require a different approach
-        execSync(`lsof -i:${port} | grep LISTEN`, { stdio: 'ignore' });
-        return true;
-    } catch {
-        return false;
-    }
+  try {
+    // macOS & Linux: use lsof; Windows would require a different approach
+    execSync(`lsof -i:${port} | grep LISTEN`, { stdio: 'ignore' });
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 /**
@@ -48,64 +48,64 @@ function isServerRunning() {
  * @returns {Promise<unknown>}
  */
 function delay(ms) {
-    return new Promise((resolve) =>
-        setTimeout(resolve, ms)
-    );
+  return new Promise((resolve) =>
+    setTimeout(resolve, ms)
+  );
 }
 
 const MAC_SERVER_PATH = './bin/server.bin';
 const WIN_SERVER_PATH = './bin/server.exe';
 
 function startServer() {
-    if (!isServerRunning()) {
-        const serverPath = process.platform === 'win32' ? WIN_SERVER_PATH : MAC_SERVER_PATH;
-        const resourcesDir = './lib/';
-        const workingDir = path.join(__dirname, '..');
-        console.log('startServer() - workingDir is ' + workingDir);
+  if (!isServerRunning()) {
+    const serverPath = process.platform === 'win32' ? WIN_SERVER_PATH : MAC_SERVER_PATH;
+    const resourcesDir = './lib/';
+    const workingDir =  path.join(__dirname, '..');
+    console.log('startServer() - workingDir is ' + workingDir);
 
-        console.log('startServer() - resourcesDir is ' + resourcesDir);
-        const env = {
-            ...process.env,
-            APP_RESOURCES_DIR: resourcesDir,
-            ROCKET_PORT: port
-        };
+    console.log('startServer() - resourcesDir is ' + resourcesDir);
+    const env = {
+      ...process.env,
+      APP_RESOURCES_DIR: resourcesDir,
+      ROCKET_PORT: port
+    };
 
-        console.log('startServer() - env is ', env);
-
-        serverProcess = spawn(serverPath, [], {
-            stdio: 'ignore',
-            detached: true,
-            env: env,
-            cwd: workingDir
-        });
-        serverProcess.unref();
-        console.log('startServer() - Server started at ' + path.join(workingDir, serverPath));
-    } else {
-        console.log(startServer() - 'Server already running.');
-    }
+    console.log('startServer() - env is ', env);
+    
+    serverProcess = spawn(serverPath, [], {
+      stdio: 'ignore',
+      detached: true,
+      env: env,
+      cwd: workingDir
+    });
+    serverProcess.unref();
+    console.log('startServer() - Server started at ' + path.join(workingDir, serverPath));
+  } else {
+    console.log(startServer() - 'Server already running.');
+  }
 }
 
 function stopServer() {
-    if (serverProcess) {
-        // Kill the process we spawned (or use another mechanism if you need gentle shutdown)
-        try {
-            process.kill(serverProcess.pid);
-            console.log('stopServer() - Server stopped.');
-        } catch (e) {
-            // It may have already exited
-            console.error('stopServer() - Server Failed to stop - process ID kill failed.');
-        }
-    } else {
-        // Optionally: kill whatever is listening on port
-        try {
-            console.log('stopServer() - Trying to stop server forcefully.');
-            execSync(`lsof -t -i:${port} | xargs kill -9`);
-            console.log('stopServer() - Server stopped forcefully.');
-        } catch {
-            // ignore if nothing is running
-            console.error(`stopServer() - Server Failed to stop - process at port ${port} ID kill failed.`);
-        }
+  if (serverProcess) {
+    // Kill the process we spawned (or use another mechanism if you need gentle shutdown)
+    try {
+      process.kill(serverProcess.pid);
+      console.log('stopServer() - Server stopped.');
+    } catch (e) {
+      // It may have already exited
+      console.error('stopServer() - Server Failed to stop - process ID kill failed.');
     }
+  } else {
+    // Optionally: kill whatever is listening on port
+    try {
+      console.log('stopServer() - Trying to stop server forcefully.');
+      execSync(`lsof -t -i:${port} | xargs kill -9`);
+      console.log('stopServer() - Server stopped forcefully.');
+    } catch {
+      // ignore if nothing is running
+      console.error(`stopServer() - Server Failed to stop - process at port ${port} ID kill failed.`);
+    }
+  }
 }
 
 function handleSetCanClose(event, newCanClose) {
@@ -170,98 +170,97 @@ function createWindow() {
 }
 
 app.whenReady().then(() => {
-    // Set a custom menu with desired app name
-    ipcMain.on('setCanClose', handleSetCanClose);
-    const isMac = process.platform === 'darwin';
-    if (isMac) {
-        const template = [
-            {
-                label: app.name, // <--- This name will show in the macOS app menu
-                submenu: [
-                    { role: 'hide' },
-                    { role: 'hideothers' },
-                    { role: 'unhide' },
-                    { type: 'separator' },
-                    { role: 'quit' }
-                ]
-            },
-            {
-                label: 'Edit',
-                submenu: [
-                    { role: 'undo' },
-                    { role: 'redo' },
-                    { type: 'separator' },
-                    { role: 'cut' },
-                    { role: 'copy' },
-                    { role: 'paste' },
-                    { role: 'pasteAndMatchStyle' },
-                    { role: 'delete' },
-                    { role: 'selectAll' }
-                ]
-            },
-            {
-                label: 'View',
-                submenu: [
-                    { role: 'reload' },
-                    { role: 'forcereload' },
-                    { role: 'toggledevtools' },
-                    { type: 'separator' },
-                    { role: 'resetzoom' },
-                    { role: 'zoomin' },
-                    { role: 'zoomout' },
-                    { type: 'separator' },
-                    { role: 'togglefullscreen' }
-                ]
-            },
-            {
-                label: 'Window',
-                submenu: [
-                    { role: 'minimize' },
-                    { role: 'zoom' },
-                    { type: 'separator' },
-                    { role: 'front' },
-                    { role: 'window' }
-                ]
-            }
-        ];
-        // Removed from the first menu section above for now:
-        /**
-              {role: 'about'},
-              {type: 'separator'},
-              {role: 'services'},
-              {type: 'separator'},
-        */
-        const menu = Menu.buildFromTemplate(template);
-        Menu.setApplicationMenu(menu);
-    }
-
-    startServer();
-    setTimeout(createWindow, 2000); // Wait 2 seconds for server to start (adjust as needed)
+  // Set a custom menu with desired app name
+  ipcMain.on('setCanClose', handleSetCanClose);
+  const isMac = process.platform === 'darwin';
+  if (isMac) {
+    const template = [
+      {
+        label: app.name, // <--- This name will show in the macOS app menu
+        submenu: [
+          {role: 'hide'},
+          {role: 'hideothers'},
+          {role: 'unhide'},
+          {type: 'separator'},
+          {role: 'quit'}
+        ]
+      },
+      {
+        label: 'Edit',
+        submenu: [
+          {role: 'undo'},
+          {role: 'redo'},
+          {type: 'separator'},
+          {role: 'cut'},
+          {role: 'copy'},
+          {role: 'paste'},
+          {role: 'pasteAndMatchStyle'},
+          {role: 'delete'},
+          {role: 'selectAll'}
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          {role: 'reload'},
+          {role: 'forcereload'},
+          {role: 'toggledevtools'},
+          {type: 'separator'},
+          {role: 'resetzoom'},
+          {role: 'zoomin'},
+          {role: 'zoomout'},
+          {type: 'separator'},
+          {role: 'togglefullscreen'}
+        ]
+      },
+      {
+        label: 'Window',
+        submenu: [
+          {role: 'minimize'},
+          {role: 'zoom'},
+          {type: 'separator'},
+          {role: 'front'},
+          {role: 'window'}
+        ]
+      }
+    ];
+    // Removed from the first menu section above for now:
+      /**
+            {role: 'about'},
+            {type: 'separator'},
+            {role: 'services'},
+            {type: 'separator'},
+      */
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+  }
+  
+  startServer();
+  setTimeout(createWindow, 2000); // Wait 2 seconds for server to start (adjust as needed)
 });
 
 app.on('window-all-closed', () => {
-    console.log('window-all-closed() - app quitting');
-    // On macOS, apps are expected to stay alive until explicitly quit
-    // but we quit anyway so server doesn't remain running
-    app.quit();
+  console.log('window-all-closed() - app quitting');
+  // On macOS, apps are expected to stay alive until explicitly quit
+  // but we quit anyway so server doesn't remain running
+  app.quit();
 });
 
 app.on('will-quit', () => {
-    console.log('will-quit() - app quitting');
-    stopServer();
+  console.log('will-quit() - app quitting');
+  stopServer();
 });
 
 app.on('before-quit', () => {
-    console.log('before-quit() - app quitting');
-    stopServer();
+  console.log('before-quit() - app quitting');
+  stopServer();
 });
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        console.log('activate() - app creating window since there are none');
-        createWindow();
-    } else {
-        console.log('activate() - app not creating window since there are already windows');
-    }
+  if (BrowserWindow.getAllWindows().length === 0) {
+    console.log('activate() - app creating window since there are none');
+    createWindow();
+  } else {
+    console.log('activate() - app not creating window since there are already windows');
+  }
 });
-
