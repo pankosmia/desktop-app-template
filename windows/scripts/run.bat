2c8fb8a8ba19f44e4a78c46f7d5a2e14b7175485
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-12-09 15:09:10 -0600

    Windows: Add run -d option for debug server

--- windows/scripts/run.bat
+++ windows/scripts/run.bat
@@ -1,11 +1,43 @@
 @echo off
 REM Run from pankosmia\[this-repo's-name]\windows\scripts directory in powershell or command by:  .\run.bat
-REM Optional argument: .\run.bat -s
-REM To pre-confirm the server is off, so as to not be asked.
+REM Optional argument: `.\run.bat -s` to pre-confirm the server is off, so as to not be asked.
+REM Optional argument: `.\run.bat -d` to run the server in debug mode.
+
+REM Do not ask if the server is off if the -s positional argument is provided in either #1 or #2
+REM Debug server if the -d positional argument is provided in either #1 or #2
+:loop
+IF "%~1"=="" (
+  goto :continue
+) ELSE IF "%~1"=="-s" (
+  set askIfOff=%~1
+) ELSE IF "%~1"=="-d" (
+  set debugServer=%~1
+)
+shift
+goto :loop
+
+:continue
+
+REM Assign default value if -s is not present
+if not defined %askIfOff (
+  set askIfOff=-yes
+)
+
+REM Assign default value if -d is not present
+if not defined %debugServer (
+  set debugServer=-no
+  set "search=local_server/target/debug"
+  set "replace=local_server/target/release"
+  set "serverType=release"
+) else if "%debugServer%"=="-d" (
+  set "search=local_server/target/release"
+  set "replace=local_server/target/debug"
+  set "serverType=debug"
+)
 
 echo.
 :choice
-IF "%~1"=="-s" (
+IF "%askIfOff%"=="-s" (
   goto :server_off
 ) ELSE (
   set /P c=Is the server off? [Y/n]: 
@@ -26,8 +58,10 @@ exit
 
 :server_off
 
+REM Identify if app_setup has already been run, and run it anything is missing.
 if not exist ..\..\buildSpec.json set runSetup=1
 if not exist ..\..\globalBuildResources\i18nPatch.json set runSetup=1
+if not exist ..\..\globalBuildResources\product.json set runSetup=1
 if not exist ..\buildResources\setup\app_setup.json set runSetup=1
 if defined %runSetup (
   cmd /c .\app_setup.bat
@@ -37,6 +71,27 @@ if defined %runSetup (
   echo   +-----------------------------------------------------------------------------+
   echo.
 )
+
+REM Ensure buildSpec.json has the location for the indicated server build type
+@echo off
+setlocal enabledelayedexpansion
+
+set "configFile=..\..\buildSpec.json"
+set "tmpFile=..\..\buildSpec.bak"
+copy %configFile% %tmpFile%
+
+
+(for /f "tokens=*" %%a in ('type "%tmpFile%" ^| findstr /n "^"') do (
+    set "line=%%a"
+    set "line=!line:*:=!"
+
+    if defined line (
+        set "line=!line:%search%=%replace%!"
+        echo(!line!
+    ) else echo.
+)) > "%configFile%"
+
+endlocal
 
 REM set port environment variables
 set ROCKET_PORT=19119
@@ -49,7 +104,7 @@ if not exist ..\build (
   echo "Assembling build environment"
   node build.js
 )
-echo "Running..."
+echo "Running with local server in %serverType% mode..."
 cd ..\build
 SET APP_RESOURCES_DIR=.\lib\
 start "" ".\bin\server.exe"
