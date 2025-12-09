19251c54f83120c3efc506ba5e55686251e0ebbc
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-12-09 15:04:43 -0600

    Windows: support coexistence of both release and debug servers

--- windows/scripts/build_server.bat
+++ windows/scripts/build_server.bat
@@ -3,18 +3,6 @@ REM Run from pankosmia\[this-repo's-name]\windows\scripts directory in powershel
 REM Git hub actions use this with optional arguments of: .\build_server.bat -c
 REM because it is a brand new clean environment without need for any prior build to be removed.
 
-if not exist ..\..\buildSpec.json set runSetup=1
-if not exist ..\..\globalBuildResources\i18nPatch.json set runSetup=1
-if not exist ..\buildResources\setup\app_setup.json set runSetup=1
-if defined %runSetup (
-  cmd /c .\app_setup.bat
-  echo.
-  echo   +-----------------------------------------------------------------------------+
-  echo   ^| Config files were rebuilt by `./app_setup.bsh` as one or more were missing. ^|
-  echo   +-----------------------------------------------------------------------------+
-  echo.
-)
-
 REM Build without cleaning if the -c positional argument is provided in either #1 or #2 or #3
 REM Do not ask if the server is off if the -s positional argument is provided in either #1 or #2 or #3
 REM Debug server if the -d positional argument is provided in either #1 or #2 or #3
@@ -49,10 +37,48 @@ if not defined %debugServer (
   set buildCommand=cargo build --release
   set "search=local_server/target/debug"
   set "replace=local_server/target/release"
+  set "serverType=release"
 ) else if "%debugServer%"=="-d" (
   set buildCommand=cargo build
   set "search=local_server/target/release"
   set "replace=local_server/target/debug"
+  set "serverType=debug"
+)
+
+echo.
+:choice
+IF "%askIfOff%"=="-s" (
+  goto :server_off
+) ELSE (
+  set /P c=Is the server off? [Y/n]: 
+)
+if /I "%c%" EQU "" goto :server_off
+if /I "%c%" EQU "Y" goto :server_off
+if /I "%c%" EQU "N" goto :server_on
+echo "%c%" is not a valid response. Please type y or 'Enter' to continue or 'n' to quit.
+goto :choice
+
+:server_on
+echo.
+echo      Exiting...
+echo.
+echo      If the server is on, turn it off by exiting the terminal window or app where it is running, then re-run this script.
+echo.
+exit
+
+:server_off
+
+if not exist ..\..\buildSpec.json set runSetup=1
+if not exist ..\..\globalBuildResources\i18nPatch.json set runSetup=1
+if not exist ..\..\globalBuildResources\product.json set runSetup=1
+if not exist ..\buildResources\setup\app_setup.json set runSetup=1
+if defined %runSetup (
+  cmd /c .\app_setup.bat
+  echo.
+  echo   +-----------------------------------------------------------------------------+
+  echo   ^| Config files were rebuilt by `./app_setup.bsh` as one or more were missing. ^|
+  echo   +-----------------------------------------------------------------------------+
+  echo.
 )
 
 REM Ensure buildSpec.json has the location for the indicated server build type
@@ -76,25 +102,20 @@ copy %configFile% %tmpFile%
 
 endlocal
 
-REM Do not clean if the -c positional argument is provided
-IF NOT "%buildWithoutClean%"=="-c" (
-  REM Do not ask if the server is off if the -s positional argument is provided
-  IF "%askIfOff%"=="-s" (
-    call .\clean.bat -s
-  ) ELSE (
-    call .\clean.bat
-  )
+REM Build the rust server of the specified build type
+echo "Building local %serverType% server at /%replace% ..."
+cd ..\..\local_server
+echo "%buildCommand%"
+%buildCommand%
+cd ..\windows\scripts
+
+REM Clean the build environment
+if exist ..\build (
+  echo Removing last build environment
+  rmdir ..\build /s /q
 )
 
-if not exist ..\..\local_server\target\release\local_server.exe (
-  if not exist ..\..\local_server\target\debug\local_server.exe (
-    echo "Building local server..."
-    cd ..\..\local_server
-    echo "%buildCommand%"
-    %buildCommand%
-    cd ..\windows\scripts
-  )
-)
+REM Assemble the build environment
 if not exist ..\build (
   echo "Assembling build environment..."
   node build.js
